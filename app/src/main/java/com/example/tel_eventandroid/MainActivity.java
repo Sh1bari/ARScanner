eef2371cf7c326e200d84cfa61b22d02a2ff03f6
TheKelp1e <vova_krasnov_2004@mail.ru>
2023-12-08 01:36:26 +0300

    scanner

--- app/src/main/java/com/example/tel_eventandroid/MainActivity.java
+++ app/src/main/java/com/example/tel_eventandroid/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.tel_eventandroid;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
+import android.graphics.*;
+import android.media.Image;
+import android.os.*;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.Preview;
+import androidx.camera.core.*;
 import androidx.camera.extensions.HdrImageCaptureExtender;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -27,21 +24,26 @@ import com.example.tel_eventandroid.models.MessageResponse;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.gson.Gson;
 import okhttp3.*;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int REQUEST_CODE_PERMISSIONS = 5555;
+    private Handler captureHandler = new Handler(Looper.getMainLooper());
     private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA"};
 
-
-    private final int SUSPENSION_TIME = 3000;
     PreviewView mPreviewView;
-    public boolean isProcess;
-    ImageCapture imageCapture;
+
+    private TextView infoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,126 +52,13 @@ public class MainActivity extends AppCompatActivity {
 
         mPreviewView = findViewById(R.id.camera);
 
+        infoTextView = findViewById(R.id.info);
+
         if(allPermissionsGranted()){
             startCamera();
         }else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
-        /*CustomDialog customDialog = new CustomDialog(MainActivity.this, "Пример сообщения");
-        customDialog.show();*/
-        //get();
-        /*CharSequence text = "Hello toast!";
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(this,
-                HtmlCompat.fromHtml("<font color='red'>custom toast message</font>", HtmlCompat.FROM_HTML_MODE_LEGACY),
-                Toast.LENGTH_LONG);
-        SpannableString str = new SpannableString("Custom toast");
-        toast.show();*/
-        // Get your custom_toast.xml ayout
-    }
-    public void get(String idUser){
-        OkHttpClient client = new OkHttpClient();
-
-
-        Request request = new Request.Builder()
-                .url("http://31.129.105.53:8082/api/check/" + idUser + "/1")
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @SuppressLint("SetTextI999n")
-            @Override
-            public void onFailure(Call call, IOException e) {
-                TextView textField = findViewById(R.id.info);
-                textField.setText("Нет интернет соединения");
-                e.printStackTrace();
-            }
-
-            @SuppressLint("SetTextI999n")
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try (ResponseBody responseBody = response.body()) {
-                    if (!response.isSuccessful()) {
-                        throw new IOException("Запрос к серверу не был успешен: " +
-                                response.code() + " " + response.message());
-                    }else{
-                        TextView textField = findViewById(R.id.info);
-                        String json = responseBody.string();
-                        Gson gson = new Gson();
-                        MessageResponse msg = gson.fromJson(json, MessageResponse.class);
-                        switch (msg.getStatus()){
-                            case "REGISTERED" : {
-                                runOnUiThread(() -> {
-                                    LayoutInflater inflater = getLayoutInflater();
-                                    View layout = inflater.inflate(R.layout.green_toast,
-                                            (ViewGroup) findViewById(R.id.custom_toast_container));
-
-                                    TextView text = (TextView) layout.findViewById(R.id.text);
-                                    text.setText("ПРОПУСК");
-
-                                    Toast toast = new Toast(getApplicationContext());
-                                    toast.setDuration(Toast.LENGTH_LONG);
-                                    toast.setView(layout);
-                                    toast.show();
-                                });
-                                break;
-                            }
-                            case "CONFIRMED" : {
-                                runOnUiThread(() -> {
-                                    LayoutInflater inflater = getLayoutInflater();
-                                    View layout = inflater.inflate(R.layout.orange_toast,
-                                            (ViewGroup) findViewById(R.id.custom_toast_container));
-
-                                    TextView text = (TextView) layout.findViewById(R.id.text);
-                                    text.setText("ПРОПУСК");
-
-                                    Toast toast = new Toast(getApplicationContext());
-                                    toast.setDuration(Toast.LENGTH_LONG);
-                                    toast.setView(layout);
-                                    toast.show();
-                                });
-                                break;
-                            }
-                            case "USER EMPTY" :
-                            case "NOT REGISTERED" : {
-                                runOnUiThread(() -> {
-                                    LayoutInflater inflater = getLayoutInflater();
-                                    View layout = inflater.inflate(R.layout.red_toast,
-                                            (ViewGroup) findViewById(R.id.custom_toast_container));
-
-                                    TextView text = (TextView) layout.findViewById(R.id.text);
-                                    text.setText("ОТКАЗАНО");
-
-                                    Toast toast = new Toast(getApplicationContext());
-                                    toast.setDuration(Toast.LENGTH_LONG);
-                                    toast.setView(layout);
-                                    toast.show();
-                                });
-                                break;
-                            }
-                            case "ERROR" : {
-                                runOnUiThread(() -> {
-                                    LayoutInflater inflater = getLayoutInflater();
-                                    View layout = inflater.inflate(R.layout.red_toast,
-                                            (ViewGroup) findViewById(R.id.custom_toast_container));
-
-                                    TextView text = (TextView) layout.findViewById(R.id.text);
-                                    text.setText("FATAL ERROR");
-
-                                    Toast toast = new Toast(getApplicationContext());
-                                    toast.setDuration(Toast.LENGTH_LONG);
-                                    toast.setView(layout);
-                                    toast.show();
-                                });
-                                break;
-                            }
-                        }
-                        textField.setText(msg.getMessage());
-                    }
-                }
-            }
-        });
-
     }
 
     private boolean allPermissionsGranted(){
@@ -194,50 +83,124 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void qRCodeHandler(String qrCodeText){
-        Context context = this;
-        //runOnUiThread(()-> Toast.makeText(context, qrCodeText, Toast.LENGTH_LONG).show());
-        runOnUiThread(() -> {
-            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-            vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE));
-            get(qrCodeText);
-        });
-
-        new Thread(()->{
-            try {
-                Thread.sleep(SUSPENSION_TIME);
-            } catch (InterruptedException e){
-                e.printStackTrace();
-            }
-            isProcess = false;
-        }).start();
-    }
-
-    void bindPreview(@NonNull ProcessCameraProvider cameraProvider){
+    @SuppressLint("UnsafeExperimentalUsageError")
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
-        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder().build();
-        imageAnalysis.setAnalyzer(Executors.newFixedThreadPool(1), new QRCodeDecoder(this));
-
-        ImageCapture.Builder builder = new ImageCapture.Builder();
-        HdrImageCaptureExtender hdrImageCaptureExtender = HdrImageCaptureExtender.create(builder);
-
-        if(hdrImageCaptureExtender.isExtensionAvailable(cameraSelector)){
-            hdrImageCaptureExtender.enableExtension(cameraSelector);
-        }
 
         Preview preview = new Preview.Builder().build();
-
-        imageCapture = builder
-                .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
+        preview.setSurfaceProvider(mPreviewView.createSurfaceProvider());
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
                 .build();
 
-        preview.setSurfaceProvider(mPreviewView.createSurfaceProvider());
+        imageAnalysis.setAnalyzer(Executors.newSingleThreadExecutor(), (imageProxy) -> {
+            // Получение данных из ImageProxy
+            try {
+                // Вызов функции для отправки на сервер
+                Thread.sleep(1000);
+                sendPhotoBytesToServer(toBitmap(imageProxy.getImage()));
 
-        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector,preview,imageAnalysis, imageCapture);
+                // Завершение анализа
+                imageProxy.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageAnalysis);
+
+    }
+    public Bitmap rotateBitmap(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+    private Bitmap toBitmap(Image image) {
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+        //U and V are swapped
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
+
+        byte[] imageBytes = out.toByteArray();
+        Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+        return rotateBitmap(bitmap, 90);
+    }
+    private void sendPhotoBytesToServer(Bitmap image){
+        runOnUiThread(() -> {
+            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE));
+        });
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        // Компрессия Bitmap в формат JPEG с качеством 100 (максимальное качество)
+        image.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        byte[] bitmapData = outputStream.toByteArray();
 
 
+        MultipartBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("image", "image.jpg", RequestBody.create(MediaType.parse("image/jpeg"), bitmapData))
+                .build();
+
+        // Создаем запрос
+        Request request = new Request.Builder()
+                .url("http://62.217.183.148:5000/predict")
+                .post(requestBody)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+
+        try {
+            client.newCall(request).enqueue(new Callback() {
+                @SuppressLint("SetTextI18n")
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        String responseBody = response.body().string();
+                        try {
+                            // Преобразование строки ответа в объект JSON
+                            JSONObject jsonResponse = new JSONObject(responseBody);
+
+                            // Получение значения "prediction" из JSON-объекта
+                            boolean prediction = jsonResponse.getBoolean("prediction");
+
+                            // Теперь у вас есть значение "prediction", которое вы можете использовать по вашему усмотрению
+                            if (prediction) {
+                                infoTextView.setText("Модель предсказала: true");
+                            } else {
+                                infoTextView.setText("Модель предсказала: false");
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        System.out.println("Ошибка при отправке данных: " + response.code() + " - " + response.message());
+                    }
+                }
+
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void startCamera(){
